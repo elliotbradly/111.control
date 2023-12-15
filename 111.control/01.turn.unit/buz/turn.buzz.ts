793e49ec2b300229294e6566eb62a019d6a9bb6f
elliotbradly <elliotbradly@gmail.com>
2023-12-14 21:53:10 -0500

    getting ready to update turn

--- 111.control/01.turn.unit/buz/turn.buzz.ts
+++ 111.control/01.turn.unit/buz/turn.buzz.ts
@@ -1,10 +1,39 @@
-export const initTurn = (cpy: TurnModel, bal:TurnBit, ste: State) => {
- debugger
- return cpy;
+
+import * as ActMnu from "../../98.menu.unit/menu.action";
+
+import * as ActTrn from "../../01.turn.unit/turn.action";
+
+import * as ActCol from "../../97.collect.unit/collect.action";
+import * as ActBus from "../../99.bus.unit/bus.action";
+
+import * as ActVrt from "../../act/vurt.action"
+import * as ActDsk from "../../act/disk.action"
+import * as ActPvt from "../../act/pivot.action";
+
+import * as ActCrd from "../../act/cardano.action";
+import * as ActBlk from "../../act/block.action";
+
+var bit, val, idx, dex, lst, dat, src;
+
+
+
+export const initTurn = async (cpy: TurnModel, bal: TurnBit, ste: State) => {
+
+
+  bit = await ste.hunt(ActBlk.OPEN_BLOCK, { idx: 'blk00' })
+
+
+  return cpy;
 };
 
-export const updateTurn = (cpy: TurnModel, bal:TurnBit, ste: State) => {
- return cpy;
+export const updateTurn = async (cpy: TurnModel, bal: TurnBit, ste: State) => {
+
+
+
+
+  bit = await ste.hunt(ActBlk.WRITE_BLOCK, { idx: 'blk00' })
+
+  return cpy;
 };
 
 
---
+++ 111.control/act/block.action.ts
@@ -0,0 +1,10 @@
+export const INIT_BLOCK = "[Block action] Init Block";
+export const UPDATE_BLOCK = "[Block action] Update Block";
+export const READ_BLOCK = "[Read action] Read Block";
+export const WRITE_BLOCK = "[Write action] Write Block";
+export const REMOVE_BLOCK = "[Remove action] Remove Block";
+export const DELETE_BLOCK = "[Delete action] Delete Block";
+export const CREATE_BLOCK = "[Create action] Create Block";
+export const NOW_BLOCK = "[Now action] Now Block";
+export const OPEN_BLOCK = "[Open action] Open Block";
+export const HASH_BLOCK = "[Hash action] Hash Block";
